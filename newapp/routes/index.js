ac6323db01a1d028b65167b142c761e8637dbde5
gautam yadav <gy77044@gmail.com>
2024-03-03 21:25:20 +0530

    cookies create read delete

--- newapp/routes/index.js
+++ newapp/routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require("./users")
+// create coookes
+router.get("/", function (req, res) {
+  res.cookie("age", 25)
+  res.render("index")
+})
+//read cookies
+router.get("/read", function (req, res) {
+  console.log(req.cookies.age)
+  res.send("check")
+})
+router.get("/delete", function (req, res) {
+  res.clearCookie("age")
+  res.send("clear hogai")
+})
 
-// router.get("/", function (req, res) {
-//   res.render("index")
-// })
+
 
 // create
 // router.get("/create", async function (req, res) {
@@ -37,24 +49,24 @@ const userModel = require("./users")
 //   res.send(deleteduser)
 // })
 
-router.get("/", function (req, res) {
-  req.session.ban = true;
-  res.render("index")
-})
-router.get("/checkban", function (req, res) {
-  if (req.session.ban === true) {
-    res.send("Your are banned")
+// router.get("/", function (req, res) {
+//   req.session.ban = true;
+//   res.render("index")
+// })
+// router.get("/checkban", function (req, res) {
+//   if (req.session.ban === true) {
+//     res.send("Your are banned")
 
-  } else {
-    res.send("not banned")
+//   } else {
+//     res.send("not banned")
 
-  }
-})
-router.get("/removeban", function (req, res) {
-  req.session.destroy(function (err){
- if(err) throw err;
- res.send("ban removed")
-    
-  }) 
-})
+//   }
+// })
+// router.get("/removeban", function (req, res) {
+//   req.session.destroy(function (err){
+//  if(err) throw err;
+//  res.send("ban removed")
+
+//   }) 
+// })
 module.exports = router;
